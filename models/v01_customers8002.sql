ea3a30e771888d6421b0dcc0989b38d1b3c6fe4e
suresh dev <suresh.dbdeveloper@gmail.com>
2026-02-14 23:42:33 +0000

    commit

--- models/v01_customers8002.sql
+++ models/v01_customers8002.sql
@@ -8,6 +8,6 @@ select
 cutomer_id as customer_id,
 first_name,
 lastname
-from {{source('s1','customers')}} where customer_id=1
+from {{source('src1','customer')}} where customer_id=1
 )
 select * from customers
